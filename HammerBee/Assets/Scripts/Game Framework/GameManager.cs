34621d6f0da4328f9d07fc779fa44bfa9ee96953
Joshua Lawrence Bierach-Glavich <jglavich@sae.au>
2016-08-26 20:34:30 +1000

    GameManager + goodies

---
+++ HammerBee/Assets/Scripts/Game Framework/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game
+{
+    using System.Collections.Generic;
+
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager instance;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            InitGame();
+        }
+
+        void InitGame()
+        {
+
+        }
+
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
