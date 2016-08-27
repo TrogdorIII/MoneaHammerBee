3146f7c373f4f1bbb08042fd023733c5bf494ebd
Joshua Lawrence Bierach-Glavich <jglavich@sae.au>
2016-08-27 15:58:15 +1000

    decal pls boss (HammerHitDecal prefab looks cute mate)

--- HammerBee/Assets/Scripts/PlayerControllers/DecalPainter.cs
+++ HammerBee/Assets/Scripts/PlayerControllers/DecalPainter.cs
@@ -17,10 +17,7 @@ public class DecalPainter : MonoBehaviour
         {
             RaycastHit hit;
             Physics.Raycast(transform.position + transform.forward * 0.5f, transform.forward, out hit, 100f);
-            Instantiate(decalObject, hit.point, Quaternion.FromToRotation(Vector3.forward, hit.normal));
-            print(hit.normal);
-            print(hit.normal * Mathf.Rad2Deg);
-            //print(new Vector3(RoundBetween(hit.normal.x * Mathf.Rad2Deg, hit.normal.x * Mathf.Rad2Deg % 90f, (hit.normal.x * Mathf.Rad2Deg % 90f) + 90f), RoundBetween(hit.normal.y * Mathf.Rad2Deg, hit.normal.y * Mathf.Rad2Deg % 90f, (hit.normal.y * Mathf.Rad2Deg % 90f) + 90f), RoundBetween(hit.normal.z * Mathf.Rad2Deg, hit.normal.z * Mathf.Rad2Deg % 90f, (hit.normal.z * Mathf.Rad2Deg % 90f) + 90f)));
+            Instantiate(decalObject, hit.point + (hit.normal * 0.01f), Quaternion.FromToRotation(Vector3.up, hit.normal));
         }
         
         transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
