cb055e2e6627b33b10a7077b9cb5185137e0ce12
Joshua Lawrence Bierach-Glavich <jglavich@sae.au>
2016-08-28 13:16:08 +1000

    scores and score screen

---
+++ HammerBee/Assets/Scripts/Menu/ScoreMenuManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ScoreMenuManager : MonoBehaviour
+{
+
+    public Button startText;
+    public Button exitText;
+    public Text scoreText;
+
+    void Awake()
+    {
+        scoreText.text = PlayerPrefs.GetFloat("score").ToString();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+
+    }
+
+    public void ExitPress()
+    {
+        startText.enabled = false;
+        exitText.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        Destroy(Game.GameManager.instance.gameObject);
+        SceneManager.LoadScene("House");
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("ScoreScreen");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
