8af0e1408d268c7efea234508f67fb62e826253b
Joshua Lawrence Bierach-Glavich <jglavich@sae.au>
2016-08-28 15:26:25 +1000

    score menu goes to credits

--- HammerBee/Assets/Scripts/Menu/ScoreMenuManager.cs
+++ HammerBee/Assets/Scripts/Menu/ScoreMenuManager.cs
@@ -45,7 +45,7 @@ public class ScoreMenuManager : MonoBehaviour
 
     public void ExitToMainMenu()
     {
-        SceneManager.LoadScene("ScoreScreen");
+        SceneManager.LoadScene("Credits");
     }
 
     public void ExitGame()
